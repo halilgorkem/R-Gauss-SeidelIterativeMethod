d0f9c537bdb93e5ce2e85b79028d5d1a2879eaea
Halil Gorkem Herek <63462353+halilgorkem@users.noreply.github.com>
2022-11-01 11:40:11 +0300

    Gauss-Seidel Yinelemeli Yöntem

---
+++ Gauss-SeidelIterativeMethod.R
@@ -0,0 +1,48 @@
+A <- matrix(c(10,-1,2,0,
+              -1,11,-1,3,
+              2,-1,10,-1,
+              0,3,-1,8), nrow = 4, byrow = T)
+
+b <-c(6,25,-11,15)
+
+GaussSeidel <-  function(A, b, TOL = 1e-5, maxIter = 100)
+{
+  n <- nrow(A)
+  iter <- 1
+  x <- rep(0,n)
+  x0 <- rep(0,n)
+  X <- data.frame(c(x0))
+  #step2
+  while(iter <= maxIter)
+  {
+    #step3
+    x[1] <- (b[1] - sum(A[1,2:n] * x0[2:n]))/A[1,1]
+    for(i in 2:(n-1))
+    {
+      x[i] <- (b[i] - sum(A[i,1:(i-1)]*x[1:(i-1)]) - sum(A[i,(i+1):n]*x0[(i+1):n]))/A[i,i]
+    }
+    
+    #n.adım
+    x[n] <- (b[n] - sum(A[n,1:(n-1)]*x0[1:(n-1)]))/A[n,n]
+    
+    X[,iter+1] <- x
+    #step4
+    if(norm((x-x0), type = "2") < TOL)
+    {
+      print("Başarılı")
+      return(list(x = as.vector(x), X, NbIter = iter))
+    }
+    #step5
+    iter <- iter + 1
+    
+    #step6
+    x0 <- x
+    
+    #step7
+    print(x)
+    stop("Maximum iterasyon sayısına ulaşıldı.")
+    
+  }
+}
+GaussSeidel(A,b)
+
