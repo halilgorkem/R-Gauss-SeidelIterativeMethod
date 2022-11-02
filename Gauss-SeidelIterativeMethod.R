8bc9bf0e9047d4ee9a59051ce47f12e73d787239
Halil Gorkem Herek <63462353+halilgorkem@users.noreply.github.com>
2022-11-02 13:37:37 +0300

    Gauss Seidell metodundaki hataların düzeltilmesi

--- Gauss-SeidelIterativeMethod.R
+++ Gauss-SeidelIterativeMethod.R
@@ -13,35 +13,30 @@ GaussSeidel <-  function(A, b, TOL = 1e-5, maxIter = 100)
   x0 <- rep(0,n)
   X <- data.frame(c(x0))
   #step2
-  while(iter <= maxIter)
-  {
-    #step3
-    x[1] <- (b[1] - sum(A[1,2:n] * x0[2:n]))/A[1,1]
-    for(i in 2:(n-1))
-    {
-      x[i] <- (b[i] - sum(A[i,1:(i-1)]*x[1:(i-1)]) - sum(A[i,(i+1):n]*x0[(i+1):n]))/A[i,i]
+  while (iter <= maxIter) {
+    
+    # Adım 3
+    x[1] <- (b[1] - sum(A[1,2:n] * x0[2:n])) / A[1,1]
+    
+    for(i in 2:(n-1)){
+      x[i] <- (b[i] - sum(A[i,1:(i-1)] * x[1:(i-1)]) - sum(A[i,(i+1):n] * x0[(i+1):n])) / A[i,i]
     }
     
-    #n.adım
-    x[n] <- (b[n] - sum(A[n,1:(n-1)]*x0[1:(n-1)]))/A[n,n]
+    x[n] <- (b[n] - sum(A[n,1:(n-1)] * x[1:(n-1)])) / A[n,n]
     
     X[,iter+1] <- x
-    #step4
-    if(norm((x-x0), type = "2") < TOL)
-    {
-      print("Başarılı")
-      return(list(x = as.vector(x), X, NbIter = iter))
+    
+    # Adım 4
+    if (norm((x-x0), type = "2") < TOL){
+      print("Algoritma başarılı...")
+      return(list(x = as.vector(x), X, NbIter = iter)) 
     }
-    #step5
+    
+    # Adım 5
     iter <- iter + 1
     
-    #step6
+    # Adım 6
     x0 <- x
-    
-    #step7
-    print(x)
-    stop("Maximum iterasyon sayısına ulaşıldı.")
-    
   }
 }
 GaussSeidel(A,b)
